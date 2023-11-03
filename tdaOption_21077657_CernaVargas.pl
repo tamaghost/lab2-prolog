65b76be3b64a1bee7eb4fbbfb231897d02c7c19a
Tamara Cerna <tamara.cernavargas@usach.cl>
2023-11-03 20:16:14 -0300

    edicion de tda option y flow: creacion de getter

--- tdaOption_21077657_CernaVargas.pl
+++ tdaOption_21077657_CernaVargas.pl
@@ -1,1 +1,13 @@
-option(CodeOp, Mensaje, ChatbotCodeLink, InitialFlowCodeLink, Keyword,[CodeOp, Mensaje, ChatbotCodeLink, InitialFlowCodeLink, [Keyword]]).
+option(CodeOp, Mensaje, ChatbotCodeLink, InitialFlowCodeLink, Keyword,[CodeOp, Mensaje, ChatbotCodeLink, InitialFlowCodeLink, Keyword]).
+
+getMensajeOption(Option, Mensaje) :-
+    option(_,Mensaje,_,_,_, Option).
+
+getKeywordOption(Option, ListKeywords) :-
+    option(_,_,_,_,ListKeywords, Option).
+
+listaMensajeOption([], [], []).  % Caso base: cuando la lista está vacía, la lista de mensajes también es vacía.
+listaMensajeOption([Cabeza|Cola], ListaMensajes) :-
+    getMensajeOption(Cabeza, Mensaje),  % Obtener el mensaje de la cabeza
+    listaMensajeOption(Cola, RestoMensajes),  % Llamada recursiva para procesar la cola
+    ListaMensajes = [Mensaje | RestoMensajes].  % Construir la lista de mensajes
