229b42fc1d130f6673f808cabb9ff9984dcced45
Tamara Cerna <tamara.cernavargas@usach.cl>
2023-11-07 21:26:02 -0300

    avance en tda system

--- tdaSystem_21077657_CernaVargas.pl
+++ tdaSystem_21077657_CernaVargas.pl
@@ -1,12 +1,29 @@
-system(Users, Name, InitialChatbotCodeLink, Chatbot, [Users, Name, InitialChatbotCodeLink, Chatbot]).
+system(Users, Name, InitialChatbotCodeLink, Chatbot, ChatHistory, [Users, Name, InitialChatbotCodeLink, Chatbot, ChatHistory]).
+
+getUserSystem(System, User):-
+    system(User,_,_,_,_,System).
+
+setSystemUser(System,NewUser,NewSystem):-
+    system(_, NameS, InitialChatbotCodeLinkS, ChatbotS, ChatHistoryS, System),
+    system(NewUser, NameS, InitialChatbotCodeLinkS, ChatbotS, ChatHistoryS, NewSystem).
+
+/*...*/
 
 getChatbotSystem(System, Chatbot):-
-    system(_,_,_,Chatbot,System).
+    system(_,_,_,Chatbot,_,System).
 
-setSystemChatbot(System,NewChatbot,NewSystem):-
-    system(UsersS, NameS, InitialChatbotCodeLinkS, _, System),
-    system(UsersS, NameS, InitialChatbotCodeLinkS, NewChatbot, NewSystem).
+setChatbotSystem(System,NewChatbot,NewSystem):-
+    system(UsersS, NameS, InitialChatbotCodeLinkS,_, ChatHistoryS, System),
+    system(UsersS, NameS, InitialChatbotCodeLinkS, NewChatbot, ChatHistoryS, NewSystem).
 
+getChatHistorySystem(System, Chat):-
+    system(_,_,_,_,Chat,System).
+
+setChatHistorySystem(System,NewChat,NewSystem):-
+    system(UsersS, NameS, InitialChatbotCodeLinkS, Chatbot, _, System),
+    system(UsersS, NameS, InitialChatbotCodeLinkS, Chatbot, NewChat, NewSystem).
+    
+%funciona
 systemAddChatbot(System1, Chatbot, System2):-
     getChatbotSystem(System1,ListSystem),
     member(Chatbot,ListSystem),
@@ -17,14 +34,32 @@ systemAddChatbot(System1, Chatbot, System2):-
     ListaModSC = [Chatbot|ListSystem],
     setSystemChatbot(System1,ListaModSC, System2).
 
-getUserSystem(System, User):-
-    chatbot(User,_,_,_,System).
 
-setSystemUser(System,NewUser,NewSystem):-
-    chatbot(_, NameS, InitialChatbotCodeLinkS, ChatbotS, System),
-    chatbot(NewUser, NameS, InitialChatbotCodeLinkS, ChatbotS, NewSystem).
-
+%funciona
 systemAddUser(System, User, NewSystem):-
-    getUserSystem(System, ListUsers)
+    getUserSystem(System, ListUsers),
     \+member(User,ListUsers),
     NewSystem = [User|System].
+%funciona
+systemLogin(System, User, SystemLogin):-
+    getUserSystem(System, ListUsers),
+    \+member(User,ListUsers),
+    write("no es posible iniciar secion").
+systemLogin(System, User, SystemLogin):-
+    getUserSystem(System, ListUsers),
+    member(User,ListUsers),
+    NewListUsers = [User|ListUsers],
+    setSystemUser(System, NewListUsers, SystemLogin).
+
+%funciona pero no se que tan util sea
+contarUsuario(_, [], 0).
+contarUsuario(User, [User|Resto], Resultado) :-
+    contarUsuario(User, Resto, ResultadoAnterior),
+    Resultado is ResultadoAnterior + 1.
+contarUsuario(User, [_|Resto], Resultado) :-
+    contarUsuario(User, Resto, Resultado).
+%funciona pero no se que tan util sea
+esSystemaLogin(System) :-
+    getUserSystem(System, ListUsers), % Asegúrate de que getUserSystem esté definido y funcione correctamente
+    contarUsuario(_, ListUsers, Resultado), % Busca cualquier usuario en la lista
+    Resultado == 2. % Verifica si se encontró al menos uno
