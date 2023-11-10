59775677d37a9005c5497ea60e5cf0aacd662446
Tamara Cerna <tamara.cernavargas@usach.cl>
2023-11-09 22:59:51 -0300

    modificacion tda user

--- tdaUser_21077657_CernaVargas.pl
+++ tdaUser_21077657_CernaVargas.pl
@@ -1,1 +1,9 @@
-user(NombreUser,[NombreUser]).
+user(NombreUser, ChatHistory,[NombreUser, ChatHistory]).
+
+getChatHistoryUser(User, Chat):-
+    system(_,Chat,User).
+
+setChatHistoryUser(User,NewChat,NewUser):-
+    system(Usuario,_,User).
+    system(Usuario,NewChat,NewUser).
+
