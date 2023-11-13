a806328aeda67c0785b872ac82b7ebf1055d6d43
Tamara Cerna <tamara.cernavargas@usach.cl>
2023-11-13 20:35:38 -0300

    olvide actualizar tda chatHistory

--- tdaChathistory_21077657_CernaVargas.pl
+++ tdaChathistory_21077657_CernaVargas.pl
@@ -1,5 +1,12 @@
-chatHistory(Usuario, StringUser, Chatbot, StringChatbot, Fecha, Hora,[Usuario, StringUser, Chatbot, StringChatbot, Fecha, Hora]).
+%Dominio: string x string x Chatbot x string x string
+%meta principal: chatHistory/6
+%Descripcion: Constructor de chatHistory
+chatHistory(Usuario, StringUser, Chatbot, StringChatbot, FechaHora,[Usuario, StringUser, Chatbot, StringChatbot, FechaHora]).
 
-setStringUserChatHistory(Chat, NewStringUser, NewChat):-
-    chatHistory(Usuario,_, Chatbot, StringChatbot, Fecha, Hora, Chat),
-    chatHistory(Usuario, NewStringUser, Chatbot, StringChatbot, Fecha, Hora, NewChat).
+%Dominio: ChatHistory x string x ChatHistory
+%meta principal: setStringUserChatHistory/3
+%meta secundaria: chatHistory/6, chatHistory/6
+%Descripcion: modificador de lista ChatHistory de user
+setStringUserChatHistory(Chat, StringUser, NewChat):-
+    chatHistory(Usuario,_, Chatbot, StringChatbot, FechaHora, Chat),
+    chatHistory(Usuario, NewStringUser, Chatbot, StringChatbot, FechaHora, NewChat).
